0933260cf19bde0ca1bd335d41a39b6860855d47
basil-from-brazil <vas60005596@yandex.ru>
2020-04-09 14:03:04 +0700

    feature(Lab1): добавлены классы Person и PersonList в проект ЛБ

---
+++ Lab1/ClassLibrary1/Genders.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    /// <summary>
+    /// Пол человека
+    /// </summary>
+    public enum Genders
+    {
+        Male,
+        Female
+    }
+}
+
