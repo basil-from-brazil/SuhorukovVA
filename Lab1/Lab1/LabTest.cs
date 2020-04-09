0e6a4582d0e159f16365ecc585f79edcc0c6a505
basil-from-brazil <vas60005596@yandex.ru>
2020-04-09 22:32:19 +0700

    Testing Git ignore

---
+++ Lab1/Lab1/LabTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+
+namespace Lab1
+{
+    class LabTest
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет! Тестирую git ignore");
+            Console.ReadKey();
+        }
+    }
+}
